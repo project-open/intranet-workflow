e4b920402354a12f41f11d68466e721209b7503f
Frank Bergmann <frank.bergmann@project-open.com>
2016-10-24 11:12:03 +0000

    - Workflow:   Implemented first and basic batch-approval of WF tasks

--- www/inbox-action.tcl
+++ www/inbox-action.tcl
@@ -4,19 +4,41 @@ ad_page_contract {
     Delete a specific task from an inbox
     @author Frank Bergmann <frank.bergmann@project-open.com>
 } {
-    action:multiple,optional
     task_id:multiple,optional
     { operation "" }
     return_url
 }
 
+
 set current_user_id [auth::require_login]
 set user_is_admin_p [im_is_user_site_wide_or_intranet_admin $current_user_id]
+set subsite_id [ad_conn subsite_id]
+set reassign_p [permission::permission_p -party_id $current_user_id -object_id $subsite_id -privilege "wf_reassign_tasks"]
 
-if {![info exists action]} { set action [list] }
 if {![info exists task_id]} { set task_id [list] }
 
-foreach object_id $action {
+
+# ad_return_complaint 1 "inbox-action: operation=$operation, task_id=$task_id"
+
+foreach tid $task_id {
+    set object_id ""
+    set object_type ""
+    db_0or1row task_info "
+		select	wfc.object_id,
+			o.object_type,
+			tr.workflow_key,
+			tr.transition_key,
+			tr.transition_name
+		from	wf_tasks wft,
+			wf_cases wfc,
+			wf_transitions tr,
+			acs_objects o
+		where	wft.task_id = :tid and
+			wft.case_id = wfc.case_id and
+			wfc.object_id = o.object_id and
+			wft.workflow_key = tr.workflow_key and
+			wft.transition_key = tr.transition_key
+    "
     switch $operation {
 	delete_membership {
 
@@ -32,44 +54,13 @@ foreach object_id $action {
 				select	wft.task_id
 				from	wf_tasks wft,
 					wf_cases wfc
-				where
-					wfc.object_id = :object_id
+				where	wfc.object_id = :object_id
 					and wft.case_id = wfc.case_id
 					and wft.state in ('enabled')
 			)
 	    "
 	}
-	nuke {
-	    ad_return_complaint 1 "nuke"	    
-	}
-	default {
-	    ad_return_complaint 1 "Unknown inbox action: '$operation'"
-	}
-    }
-}
 
-foreach tid $task_id {
-
-    set object_id ""
-    set object_type ""
-    db_0or1row task_info "
-		select
-			wfc.object_id,
-			o.object_type
-		from
-			wf_tasks wft,
-			wf_cases wfc,
-			acs_objects o
-		where
-			wft.task_id = :tid and
-			wft.case_id = wfc.case_id and
-			wfc.object_id = o.object_id
-    "
-
-    switch $operation {
-	delete_membership {
-	    ad_return_complaint 1 "Unknown inbox action: '$operation'"
-	}
 	nuke {
 	    switch $object_type {
 		im_ticket - im_project {
@@ -83,8 +74,56 @@ foreach tid $task_id {
 	    }
 	}
 	default {
-	    ad_return_complaint 1 "Unknown inbox action: '$operation'"
-	    ad_script_abort
+            if {$operation ne $transition_key} {
+		# Check that the user didn't apply the wrong operation on a task.
+		ad_return_complaint 1 "You can't perform '$operation' on a transition that needs '$transition_key' on task #$task_id"
+		ad_script_abort
+	    }
+
+	    # Check if the current task complies with the following conditions:
+	    # - Task is assigned to the current user, or the current user is an admin
+	    # - Task doesn't include a custom action panel
+	    # - If the task requires a yes/no decision, "yes" is selected
+	    #
+	    set msg "Batch operation '$operation' from WF inbox"
+	    set action "finish"
+
+	    # Get the list of attributes defined in this workflow
+	    set attributes [db_list wf_attributes "
+		select	attribute_name
+		from	acs_attributes
+		where	object_type = :workflow_key   
+	    "]
+	    if {[llength $attributes] > 1} { ad_return_complaint 1 "Found more than one attribute ($attributes) in workflow $workflow_key" }
+	    set attribute_name [lindex $attributes 0]
+	    set attribute_hash($attribute_name) "t"
+
+	    # Check if the current user is assigned to the task and try to self-assign if not.
+	    set current_user_assigned_p [db_string task_assigned_users "
+		select	count(*)
+		from	wf_user_tasks ut
+		where	ut.task_id = :task_id and
+			ut.user_id = :current_user_id
+	    "]
+	    if {!$current_user_assigned_p} {
+		if {!$reassign_p} {
+		    ad_return_complaint 1 "<li>[_ intranet-core.lt_You_have_insufficient_1]"
+		    return
+		} else {
+		    wf_case_add_task_assignment -task_id $task_id -party_id $current_user_id
+		}
+	    }
+
+	    set journal_id [wf_task_action \
+				-user_id $current_user_id \
+				-msg $msg \
+				-attributes [array get attribute_hash] \
+				-assignments [array get assignments] \
+				$task_id \
+				$action \
+	    ]
+
+
 	}
     }
 }
